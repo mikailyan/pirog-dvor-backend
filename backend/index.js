8dd034c25bdc52bd5b0bc7265b0abb1500249e4e
Микаил Булдаков <mika-buldakov@mail.ru>
2024-12-19 01:32:46 +0300

    new info

--- backend/index.js
+++ backend/index.js
@@ -68,14 +68,14 @@ app.post('/api/create-payment', async (req, res) => {
 });
 
 app.post('/success', async (req, res) => {
-    const {queryId, products = [], totalPrice, number, street, FIO, email, comment, DeliveryPay} = req.body;
+    const {queryId, products = [], totalPrice, number, street, FIO, email, comment, deliverypay} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {
-                message_text: `Спасибо за заказ, ${FIO}!\nЗаказ будет оплачен: ${DeliveryPay}\nЗаказ будет доставлен по адресу: ${street}\nС вами свяжутся по номеру ${number}\nОбщая сумма заказа: ${totalPrice} руб.\n` + 
+                message_text: `Спасибо за заказ, ${FIO}!\nЗаказ будет оплачен: ${deliverypay}\nЗаказ будет доставлен по адресу: ${street}\nС вами свяжутся по номеру ${number}\nОбщая сумма заказа: ${totalPrice} руб.\n` + 
                 products.map(item => `${item.title} - ${item.quantity} шт.`).join('\n')
             }
         })
